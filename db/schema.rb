34fdc6051afa177dcb4e590479a799b9e5a55b3d
root <root@localhost.localdomain>
2019-06-15 14:12:14 -0400

    fix bugs

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_06_15_154305) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "carts", force: :cascade do |t|
     t.string "name"
     t.decimal "money"
